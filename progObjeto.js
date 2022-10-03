ec0265454d5a015a003a1e23e1b7eb89e28e0f09
lara-bozzano <larabozzano@tdf.edu.ar>
2022-10-04 00:30:56 +0200

    clase e instancia de un Obj

--- progObjeto.js
+++ progObjeto.js
@@ -1,3 +1,31 @@
-//IPOD
-//Funcionalidades: prender/apagar, cambiar vol, cambiar cancion, boton inicio
-//Estados: prendido/apagado, volumen actual, estado d cancion actual, porc de bateria
+"use strict";
+//OBJETO, 3 FUNC Y 3 ESTADOS. PLANTEAR CLASE 
+exports.__esModule = true;
+//EQUIPO DE MUSICA
+//Func: prender/apagar, subir/bajar volumen, cambiar cancion/radio, pausar/play
+//Estados: prendido/apagado, volumen actual, emisora actual, cancion actual, minuto cancion, marca
+var Equipo = /** @class */ (function () {
+    //funciones
+    function Equipo(marca, volumenActual) {
+        this.marca = marca;
+        this.volumenActual = volumenActual;
+    }
+    Equipo.prototype.subirVolumen = function () {
+        this.volumenActual = this.volumenActual + 1;
+        console.log("El volumen subió a: " + this.volumenActual);
+    };
+    Equipo.prototype.bajarVolumen = function () {
+        this.volumenActual = this.volumenActual - 1;
+        console.log("El volumen bajó a: " + this.volumenActual);
+    };
+    Equipo.prototype.nombreMarca = function () {
+        console.log("El nombre de la marca es: " + this.marca);
+    };
+    return Equipo;
+}());
+var equipo = new Equipo("Samsung", 2);
+console.log(equipo.marca);
+console.log(equipo.volumenActual);
+equipo.subirVolumen();
+equipo.nombreMarca();
+equipo.bajarVolumen();
--- progObjeto.ts
+++ progObjeto.ts
@@ -1,3 +1,48 @@
-//IPOD
-//Funcionalidades: prender/apagar, cambiar vol, cambiar cancion, boton inicio
-//Estados: prendido/apagado, volumen actual, estado d cancion actual, porc de bateria
+//OBJETO, 3 FUNC Y 3 ESTADOS. PLANTEAR CLASE 
+
+import { isThisTypeNode } from "typescript";
+
+//EQUIPO DE MUSICA
+//Func: prender/apagar, subir/bajar volumen, cambiar cancion/radio, pausar/play
+//Estados: prendido/apagado, volumen actual, emisora actual, cancion actual, minuto cancion, marca
+
+class Equipo{
+    marca: string;
+    modelo: string;
+    volumenActual: number;
+    tiempoCancion: number;
+
+    //funciones
+
+    constructor(marca: string, volumenActual: number) {
+        this.marca = marca;
+        this.volumenActual = volumenActual;
+    }
+
+    subirVolumen(){
+        this.volumenActual = this.volumenActual + 1;
+        console.log("El volumen subió a: " + this.volumenActual);
+    }
+
+    bajarVolumen(){
+        this.volumenActual = this.volumenActual - 1;
+        console.log("El volumen bajó a: " + this.volumenActual);
+    }
+
+    nombreMarca(){
+        console.log("El nombre de la marca es: " + this.marca)
+    }
+}
+
+let equipo = new Equipo("Samsung", 2);
+
+console.log(equipo.marca);
+console.log(equipo.volumenActual);
+
+equipo.subirVolumen();
+equipo.nombreMarca();
+equipo.bajarVolumen();
+
+
+
+
